0ecf78cf36fef389739b2541fda3d04d5fd0f165
RoccoGibbons <roccogibbons@outlook.com>
2024-06-30 15:50:31 +0100

    Added snowman

--- snowman.py
+++ snowman.py
@@ -3,6 +3,27 @@ import random
 def main():
     difficulty = int(input("What difficulty do you want to play? \neasy: 1, medium: 2, hard: 3\n"))
     
+    word = random_word(difficulty).lower()
+    length = len(word) - 1
+    display = []
+
+    print(word)
+
+    for i in range(length):
+        display.append('-')
+    print(display)
+    print()
+
+    incorrect_guesses = 6
+
+    while incorrect_guesses != 6:
+        break
+
+    print_snowman(incorrect_guesses)
+
+
+
+def random_word(difficulty):
     dictionary = open("dictionary.txt", "r")
     
     if difficulty == 1:
@@ -16,12 +37,25 @@ def main():
 
     else:
         print("Please choose a valid difficulty")
-    
-    print(word)
 
     dictionary.close()
 
+    return word
 
-
+def print_snowman(incorrect_guesses):
+    if incorrect_guesses == 0:
+        return
+    elif incorrect_guesses == 1:
+        print("(       )\n '_____'")
+    elif incorrect_guesses == 2:
+        print(" (     )\n(       )\n '-----'")
+    elif incorrect_guesses == 3:
+        print("  (   )\n (     )\n(       )\n '-----'")
+    elif incorrect_guesses == 4:
+        print("  ('v')\n (     )\n(       )\n '-----'")
+    elif incorrect_guesses == 5:
+        print("    ('v')\n--<(  .  )>--\n  (   .   )\n   '-----'")
+    elif incorrect_guesses == 6:
+        print("     ___\n   _|___|_\n    ('v')\n--<(  .  )>--\n  (   .   )\n   '-----'") #Game over
 
 main()
