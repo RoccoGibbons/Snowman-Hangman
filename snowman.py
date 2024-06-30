c96429da7b8ff2fa4886018a81cf32483bf5ea34
RoccoGibbons <roccogibbons@outlook.com>
2024-06-30 14:55:26 +0100

    Added main python file and dictionary file

---
+++ snowman.py
@@ -0,0 +1,12 @@
+import random
+
+def main():
+    difficulty = int(input("What difficulty do you want to play? \neasy: 1, medium: 2, hard: 3\n"))
+    
+    dictionary = open("dictionary.txt", "r")
+    
+
+
+
+
+main()
