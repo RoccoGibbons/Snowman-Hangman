86f30f71a89c366111960f66f26f45a30b64b144
RoccoGibbons <roccogibbons@outlook.com>
2024-06-30 15:04:57 +0100

    Able to read random word from file

--- snowman.py
+++ snowman.py
@@ -5,6 +5,21 @@ def main():
     
     dictionary = open("dictionary.txt", "r")
     
+    if difficulty == 1:
+        word = dictionary.readlines()[random.randint(0, 539)]
+    
+    elif difficulty == 2:
+        word = dictionary.readlines()[random.randint(540, 841)]
+
+    elif difficulty == 3:
+        word = dictionary.readlines()[random.randint(842, 851)]
+
+    else:
+        print("Please choose a valid difficulty")
+    
+    print(word)
+
+    dictionary.close()
 
 
 
